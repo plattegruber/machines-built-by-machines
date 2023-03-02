0af84799cf082be7ae5975a39caa0a9fd20d1862
plattegruber <plattegruber@yahoo.com>
2023-03-01 20:35:36 -0600

    add a test

---
+++ test/main_test.dart
@@ -0,0 +1,6 @@
+import 'package:machines_built_by_machines/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('this test should pass', () => const MainApp().toString() != '');
+}
